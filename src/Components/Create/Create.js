c92685e4ae51c390970d8ad6050e776514eaf92e
athulkrishna <athulkrishna4232@gmail.com>
2022-06-27 17:25:22 +0530

    Action to sell button and view post completed

--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -1,8 +1,39 @@
-import React, { Fragment } from 'react';
-import './Create.css';
-import Header from '../Header/Header';
+import React, { Fragment, useState, useContext } from "react";
+import { FirebaseContext, AuthContext } from "../../store/Context";
+
+import "./Create.css";
+import Header from "../Header/Header";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
+  const { firebase } = useContext(FirebaseContext);
+  const history = useHistory();
+  const { user } = useContext(AuthContext);
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState(null);
+  const date = new Date();
+  const handleSubmit = () => {
+    firebase
+      .storage()
+      .ref(`/image/${image.name}`)
+      .put(image)
+      .then(({ ref }) => {
+        ref.getDownloadURL().then((url) => {
+          console.log(url);
+          firebase.firestore().collection("products").add({
+            name,
+            category,
+            price,
+            url,
+            userId: user.uid,
+            createdAt: date.toDateString(),
+          });
+          history.push("/");
+        });
+      });
+  };
   return (
     <Fragment>
       <Header />
@@ -13,6 +44,8 @@ const Create = () => {
             <br />
             <input
               className="input"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               type="text"
               id="fname"
               name="Name"
@@ -23,6 +56,8 @@ const Create = () => {
             <br />
             <input
               className="input"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
               type="text"
               id="fname"
               name="category"
@@ -31,17 +66,35 @@ const Create = () => {
             <br />
             <label htmlFor="fname">Price</label>
             <br />
-            <input className="input" type="number" id="fname" name="Price" />
+            <input
+              className="input"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              type="number"
+              id="fname"
+              name="Price"
+            />
             <br />
           </form>
           <br />
-          <img alt="Posts" width="200px" height="200px" src=""></img>
-          <form>
-            <br />
-            <input type="file" />
-            <br />
-            <button className="uploadBtn">upload and Submit</button>
-          </form>
+          <img
+            alt="Posts"
+            width="200px"
+            height="200px"
+            src={image ? URL.createObjectURL(image) : ""}
+          ></img>
+
+          <br />
+          <input
+            onChange={(e) => {
+              setImage(e.target.files[0]);
+            }}
+            type="file"
+          />
+          <br />
+          <button onClick={handleSubmit} className="uploadBtn">
+            upload and Submit
+          </button>
         </div>
       </card>
     </Fragment>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,7 @@ import firebase from "firebase";
 import { initializeApp } from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCHkaa00vmi6WwT7ENE46Jl2P6lEvrjbnw",
